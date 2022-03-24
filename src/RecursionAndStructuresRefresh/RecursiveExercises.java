7c16be65994998f7cfb3c8705a44d1638aeea42c
Geada734 <frankgeada@gmail.com>
2022-03-23 18:20:46 -0600

    More basic recursion exercises

--- src/RecursionAndStructuresRefresh/RecursiveExercises.java
+++ src/RecursionAndStructuresRefresh/RecursiveExercises.java
@@ -13,4 +13,59 @@ public class RecursiveExercises {
 		return 0;
 	};
 	
+	public int sumBetween(int start, int end) {
+		if(start<=end) {
+			return start + sumBetween(start+1, end);
+		};
+		
+		return 0;
+	};
+	
+	public int factorial(int n) {
+		if(n>0) {
+			return n * factorial(n-1);
+		};
+		
+		return 1;
+	};
+	
+	public int bunnyEars(int n) {
+		if(n>0) {
+			if(n%2==0) {
+				return 3 + bunnyEars(n-1);
+			}
+			else {
+				return 2 + bunnyEars(n-1);
+			}
+		};
+		
+		return 0;
+	};
+	
+	public int count7(int n) {
+		if(n>0) {
+			if(n%10==7) {
+				return 1 + count7((int)Math.floor(n/10));
+			}
+			else {
+				return count7((int)Math.floor(n/10));
+			}
+		};
+		
+		return 0;
+	};
+	
+	public int countX(String str) {
+		if(str.length()>0) {
+			if(str.charAt(0)=='x') {
+				return 1 + countX(str.substring(1));
+			}
+			else {
+				return countX(str.substring(1));
+			}
+		};
+		
+		return 0;
+	};
+	
 };
--- src/RecursionAndStructuresRefresh/main.java
+++ src/RecursionAndStructuresRefresh/main.java
@@ -11,6 +11,26 @@ public class main {
 	    System.out.println("Sum: ");
 	    System.out.println(sum);
 	    System.out.println("******************************");
+	    int sumBetween = recursionDriver.sumBetween(5, 10);
+	    System.out.println("Sum Between: ");
+	    System.out.println(sumBetween);
+	    System.out.println("******************************");
+	    int factorial = recursionDriver.factorial(5);
+	    System.out.println("Factorial: ");
+	    System.out.println(factorial);
+	    System.out.println("******************************");
+	    int bunnyEars = recursionDriver.bunnyEars(10);
+	    System.out.println("Bunny Ears: ");
+	    System.out.println(bunnyEars);
+	    System.out.println("******************************");
+	    int count7 = recursionDriver.count7(7717673);
+	    System.out.println("Count 7: ");
+	    System.out.println(count7);
+	    System.out.println("******************************");
+	    int countX = recursionDriver.countX("XXioxiuoixuo");
+	    System.out.println("Count X: ");
+	    System.out.println(countX);
+	    System.out.println("******************************");
 	};
 
 };
