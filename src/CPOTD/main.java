7d0828ad0e7ed0cb58293a5f8de912bb8b01f297
Geada734 <frankgeada@gmail.com>
2022-07-03 16:39:52 -0500

    Small formatting stuff

--- src/CPOTD/main.java
+++ src/CPOTD/main.java
@@ -2,6 +2,7 @@ package CPOTD;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class main {
@@ -50,7 +51,7 @@ public class main {
 	};
 	
 	public static int findIndexOfInt(int[] arr, int x) {
-		
+		// Didn't optimize this after all.
 		for(int i=0; i<arr.length; i++) {
 			if(arr[i]==x) {
 				return i;
